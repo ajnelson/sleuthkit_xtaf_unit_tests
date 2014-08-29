aeae253302850f822c33f020169449333c5e6c34
Alex Nelson <a.nelson@prometheuscomputing.com>
2014-08-29 15:39:28 -0400

    Handle potential issues with tsk3/ persisting

--- check_for_tskloaddb_segfault.sh
+++ check_for_tskloaddb_segfault.sh
@@ -2,13 +2,15 @@
 
 #This test checks for a segfault.
 
-set -e
+#set -e #Don't disable error-halts.
 set -x
 
 test -r DRIVE2_TIME_FINAL.E01
 
 #Cleanup from last test
 rm -rf build test.db
+rm -rf tsk3 #An artifact of checkouts
+git checkout -- .
 
 ./bootstrap
 ./configure --disable-java --prefix=$PWD/build
@@ -17,3 +19,10 @@ make install
 
 #Segfault here -> 'bad' commit.  Any other execution = 'good' commit, for the purpose of this test.
 build/bin/tsk_loaddb -d test.db DRIVE2_TIME_FINAL.E01
+rc=$?
+
+echo "NOTE:$(basename $0):Resetting tsk3/ dir (if it's there) for next git-bisect checkout." >&2
+rm -rf tsk3
+git checkout -- .
+
+exit $rc
